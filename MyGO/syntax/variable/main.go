615da630c0ace37008765a631e1f94335ce924dd
Jxle777 <yuanjiyang6@gmail.com>
2025-11-03 16:50:47 +0800

    Day1

---
+++ MyGO/syntax/variable/main.go
@@ -0,0 +1,28 @@
+package main
+
+var Gloal = "全局变量"
+var internal = "包内变量，私有变量"
+var (
+	first  string = "1"
+	second string = "2"
+)
+
+func main() {
+	var a int = 123 //go会类型推断
+	println(a)
+
+	var b = 234
+	println(b)
+
+	var c uint = 456
+	println(c)
+
+	var (
+		d string = "aaa"
+		e int    = 123
+	)
+	println(d, e)
+
+	f := 123
+	println(f)
+}
