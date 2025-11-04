a19cdc0c284185dfc4438c360632c90196696d6b
Jxle777 <yuanjiyang6@gmail.com>
2025-11-04 21:33:34 +0800

    Day2

--- MyGO/syntax/control/switch.go
+++ MyGO/syntax/control/switch.go
@@ -1,11 +1,24 @@
 package main
 
 func Switch(status int) {
-	switch status {
+	switch status { //带了status为对应类型的值
 	case 0:
 		println("init")
 	case 1:
 		println("running")
+	default: //可有可无
+		println("unknow")
+	}
+	println("都没有执行直接执行这句")
+}
+
+func SwitchBool(age int) {
+	switch { //不带为bool类型的值
+	case age >= 18:
+		println("成年人")
+	case age >= 12:
+		println("青年人")
 	default:
+		println("")
 	}
 }
